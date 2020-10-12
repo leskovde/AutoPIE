e6bb856be828f27f8f9151fc8929c5420d012d3a
Denis <Denis@LAPTOP-J8C4M3I8>
2020-10-12 11:11:24 +0200

    Implemented naive reduction and validation.

--- TheToolSolution/ClangDemo/TheTool.cpp
+++ TheToolSolution/ClangDemo/TheTool.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <filesystem>
 // Declares clang::SyntaxOnlyAction.
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
@@ -13,8 +14,8 @@
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Rewrite/Core/Rewriter.h"
 
-#define VARIATIONS 0
-#define STMTDEMO 0
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+
 #define SMALLFILE 1
 
 using namespace clang;
@@ -28,8 +29,10 @@ std::string TempName = "tempFile.cpp";
 Rewriter RewriterInstance;
 auto Variants = std::stack<std::string>();
 
-int CurrentLine;
+int CurrentStatementNumber;
 int CountVisitorCurrentLine;
+
+int ErrorLineNumber = 2;
 
 int Iteration = 0;
 int NumFunctions = 0;
@@ -42,50 +45,41 @@ static SourceRange GetSourceRange(const Stmt& s)
 	return {s.getBeginLoc(), s.getEndLoc()};
 }
 
-// Traverses the entire AST and reduces it via Visit functions
-class ReductionASTVisitor : public RecursiveASTVisitor<ReductionASTVisitor>
+static int GetChildrenCount(Stmt* st)
 {
-	ASTContext* astContext; // used for getting additional AST info
+	int childrenCount = 0;
 
-public:
-	virtual ~ReductionASTVisitor() = default;
-
-	explicit ReductionASTVisitor(CompilerInstance* ci)
-		: astContext(&ci->getASTContext()) // initialize private members
+	for (auto it = st->children().begin(); it != st->children().end(); ++it)
 	{
-		RewriterInstance.setSourceMgr(astContext->getSourceManager(),
-		                              astContext->getLangOpts());
+		childrenCount++;
 	}
 
-#define CALLSTMT 1
-
-#ifdef RETURNSTMT
-	virtual bool VisitStmt(Stmt* st)
+	for (auto child : st->children())
 	{
-		if (const auto ret = dyn_cast<ReturnStmt>(st))
-		{
-			const auto range = GetSourceRange(*ret);
-			RewriterInstance.RemoveText(range);
-			errs() << "** Removed ReturnStmt\n";
-		}
-
-		return true;
+		childrenCount += GetChildrenCount(child);
 	}
-#endif
-#ifdef CALLSTMT
-	virtual bool VisitStmt(Stmt* st)
+
+	return childrenCount;
+}
+
+std::string ExecCommand(const char* cmd)
+{
+	std::array<char, 128> buffer;
+	std::string result;
+	std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
+
+	if (!pipe)
 	{
-		if (const auto call = dyn_cast<CallExpr>(st))
-		{
-			const auto range = GetSourceRange(*call);
-			RewriterInstance.RemoveText(range);
-			errs() << "** Removed function call\n";
-		}
-
-		return true;
+		throw std::runtime_error("PILE ERROR: _popen() failed!");
 	}
-#endif
-};
+
+	while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+	{
+		result += buffer.data();
+	}
+
+	return result;
+}
 
 // Traverses the AST and removes a selected statement set by the CurrentLine number
 class StatementReductionASTVisitor : public RecursiveASTVisitor<StatementReductionASTVisitor>
@@ -100,9 +94,7 @@ public:
 		: astContext(&ci->getASTContext()) // initialize private members
 	{
 		SourceChanged = false;
-#ifdef VARIATIONS
 		RewriterInstance = Rewriter();
-#endif
 		RewriterInstance.setSourceMgr(astContext->getSourceManager(),
 		                              astContext->getLangOpts());
 	}
@@ -111,15 +103,31 @@ public:
 	{
 		iteration++;
 
-		if (iteration == CurrentLine)
+		if (iteration == CurrentStatementNumber)
 		{
-			// Remove non-compound statements first
-			if (st->children().empty())
+			auto numberOfChildren = GetChildrenCount(st);
+
+			const auto range = GetSourceRange(*st);
+			auto locStart = astContext->getSourceManager().getPresumedLoc(range.getBegin());
+			auto locEnd = astContext->getSourceManager().getPresumedLoc(range.getEnd());
+
+			Rewriter rewriter;
+			rewriter.setSourceMgr(astContext->getSourceManager(), astContext->getLangOpts());
+
+			outs() << "\n============================================================\n";
+			outs() << "STATEMENT NO. " << iteration << "\n";
+			outs() << "CODE: " << rewriter.getRewrittenText(range) << "\n";
+			outs() << range.printToString(astContext->getSourceManager()) << "\n";
+			outs() << "CHILDREN COUNT: " << numberOfChildren << "\n";
+			outs() << "\n============================================================\n";
+
+			if (locStart.getLine() != ErrorLineNumber && locEnd.getLine() != ErrorLineNumber)
 			{
-				const auto range = GetSourceRange(*st);
 				RewriterInstance.RemoveText(range);
 				SourceChanged = true;
 			}
+
+			CurrentStatementNumber += numberOfChildren;
 
 			return false;
 		}
@@ -128,7 +136,7 @@ public:
 	}
 };
 
-// Counts the number of statements and stores it in CountVisitorCurrentLine
+// Counts the number of expressions and stores it in CountVisitorCurrentLine
 class CountASTVisitor : public RecursiveASTVisitor<CountASTVisitor>
 {
 	ASTContext* astContext; // used for getting additional AST info
@@ -142,37 +150,18 @@ public:
 	virtual bool VisitStmt(Stmt* st)
 	{
 		CountVisitorCurrentLine++;
+
 		return true;
 	}
 };
 
-class ReductionASTConsumer final : public ASTConsumer
-{
-	ReductionASTVisitor* visitor; // doesn't have to be private
-
-public:
-	// override the constructor in order to pass CI
-	explicit ReductionASTConsumer(CompilerInstance* ci)
-		: visitor(new ReductionASTVisitor(ci)) // initialize the visitor
-	{
-	}
-
-	// override this to call our ExampleVisitor on the entire source file
-	void HandleTranslationUnit(ASTContext& context) override
-	{
-		/* we can use ASTContext to get the TranslationUnitDecl, which is
-		     a single Decl that collectively represents the entire source file */
-		visitor->TraverseDecl(context.getTranslationUnitDecl());
-	}
-};
-
-class StatementReductionASTConsumer final : public ASTConsumer
+class ExpressionReductionASTConsumer final : public ASTConsumer
 {
 	StatementReductionASTVisitor* visitor; // doesn't have to be private
 
 public:
 	// override the constructor in order to pass CI
-	explicit StatementReductionASTConsumer(CompilerInstance* ci)
+	explicit ExpressionReductionASTConsumer(CompilerInstance* ci)
 		: visitor(new StatementReductionASTVisitor(ci)) // initialize the visitor
 	{
 	}
@@ -207,36 +196,7 @@ public:
 	}
 };
 
-
-class ReduceSourceCodeAction final : public ASTFrontendAction
-{
-public:
-
-	// Prints the updated source file to the console and to the TempName file
-	void EndSourceFileAction() override
-	{
-		outs() << "FILE REDUCTION: END OF FILE ACTION:\n";
-		auto& sm = RewriterInstance.getSourceMgr();
-
-		RewriterInstance.getEditBuffer(sm.getMainFileID()).write(errs());
-
-		std::error_code errorCode;
-		raw_fd_ostream outFile(TempName, errorCode, sys::fs::F_None);
-
-		RewriterInstance.getEditBuffer(sm.getMainFileID()).write(outFile); // --> this will write the result to outFile
-		outFile.close();
-
-		outs() << "\n";
-	}
-
-	std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance& ci, StringRef file) override
-	{
-		return std::unique_ptr<ASTConsumer>(std::make_unique<ReductionASTConsumer>(&ci));
-		// pass CI pointer to ASTConsumer
-	}
-};
-
-class StatementReduceAction final : public ASTFrontendAction
+class ExpressionReduceAction final : public ASTFrontendAction
 {
 public:
 
@@ -248,7 +208,7 @@ public:
 
 		auto& sm = RewriterInstance.getSourceMgr();
 
-		outs() << "STMT REDUCTION: END OF FILE ACTION:\n";
+		outs() << "EXPR REDUCTION: END OF FILE ACTION:\n";
 		RewriterInstance.getEditBuffer(sm.getMainFileID()).write(errs());
 
 		const auto fileName = "temp/" + std::to_string(Iteration) + TempName;
@@ -264,7 +224,7 @@ public:
 
 	std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance& ci, StringRef file) override
 	{
-		return std::unique_ptr<ASTConsumer>(std::make_unique<StatementReductionASTConsumer>(&ci));
+		return std::unique_ptr<ASTConsumer>(std::make_unique<ExpressionReductionASTConsumer>(&ci));
 		// pass CI pointer to ASTConsumer
 	}
 };
@@ -298,32 +258,66 @@ static int GetStatementCount(CompilationDatabase& cd, const std::string& fileNam
 }
 
 // Removes lineNumber-th statement in the fileName source code file
-static void ReduceStatement(CompilationDatabase& cd, const std::string& fileName, const int lineNumber)
+static void ReduceStatement(CompilationDatabase& cd, const std::string& fileName, const int expressionNumber)
 {
-	CurrentLine = lineNumber;
+	CurrentStatementNumber = expressionNumber;
 
 	ClangTool tool(cd, fileName);
-	auto result = tool.run(newFrontendActionFactory<StatementReduceAction>().get());
+	auto result = tool.run(newFrontendActionFactory<ExpressionReduceAction>().get());
 }
 
 extern cl::OptionCategory MyToolCategory;
+
+bool Validate(const char* const userInputError, const std::filesystem::directory_entry& entry)
+{
+	outs() << "ENTRY: " << entry.path().string() << "\n";
+	std::string compileCommand = "g++ -o temp\\" + entry.path().filename().replace_extension(".exe").string() + " " +
+		entry.path().string() + " 2>&1";
+
+	auto compilationResult = ExecCommand(compileCommand.c_str());
+	outs() << "COMPILATION: " << compilationResult << "\n";
+
+	if (compilationResult.find("error") == std::string::npos)
+	{
+		std::string debugCommand = "gdb --batch -ex run temp\\" + entry
+		                                                          .path().filename().replace_extension(".exe").string()
+			+ " 2>&1";
+
+		auto debugResult = ExecCommand(debugCommand.c_str());
+		outs() << "DEBUG: " << debugResult << "\n";
+
+		if (debugResult.find(userInputError) != std::string::npos)
+		{
+			copy_file(entry, "result.cpp");
+			return true;
+		}
+	}
+	return false;
+}
 
 // Generates all possible variations of the supplied source code
 // Call:
 // > TheTool.exe [path to a cpp file] --
 int main(int argc, const char** argv)
 {
+	const auto userInputError = "std::invalid_argument";
 	// parse the command-line args passed to the code
 	CommonOptionsParser op(argc, argv, MyToolCategory);
 
+	// clean the temp directory
+	std::filesystem::remove_all("temp/");
+	std::filesystem::create_directory("temp");
+
 #if SMALLFILE
-	const std::string fileName = R"(C:\Users\Denis\llvm\llvm-project\TestSource.cpp)";
+	const std::string fileName = R"(C:\Users\Denis\llvm\llvm-project\TestSourceException.cpp)";
 #else
 	// TOOL CREATION & COMPILATION OVERHEAD DEMO
 	const std::string fileName = R"(C:\Users\Denis\source\repos\Diacritics\Diacritics\Diacritics.cpp)";
 #endif
 
-#if VARIATIONS
+	const auto originalExpressionCount = GetStatementCount(op.getCompilations(), *op.getSourcePathList().begin());
+	const auto acceptableRatio = 0.85;
+
 	SourceChanged = false;
 	Variants.push(*op.getSourcePathList().begin());
 
@@ -333,30 +327,31 @@ int main(int argc, const char** argv)
 		auto currentFile = Variants.top();
 		Variants.pop();
 
-		const auto lineCount = GetStatementCount(op.getCompilations(), currentFile);
+		const auto expressionCount = GetStatementCount(op.getCompilations(), currentFile);
+
+		if ((static_cast<double>(expressionCount) / originalExpressionCount) < acceptableRatio)
+		{
+			continue;
+		}
 
 		// Remove each statement once and push a new file without that statement onto the stack (done inside the FrontEndAction)
-		for (auto i = 1; i <= lineCount; i++)
+		for (auto i = 1; i <= expressionCount; i++)
 		{
 			outs() << "Iteration: " << std::to_string(Iteration) << "\n\n";
 			ReduceStatement(op.getCompilations(), currentFile, i);
 			Iteration++;
 		}
 	}
-#elif STMTDEMO
-	const auto lineCount = GetStatementCount(op.getCompilations(), fileName);
 
-	// Remove each statement once and push a new file without that statement onto the stack (done inside the FrontEndAction)
-	for (auto i = 1; i <= lineCount; i++)
-	{
-		outs() << "\n\nIteration: " << std::to_string(Iteration) << "\n\n";
-		ReduceStatement(op.getCompilations(), fileName, i);
-		Iteration++;
-	}
-#else
-	ClangTool tool(op.getCompilations(), fileName);
-	auto result = tool.run(newFrontendActionFactory<ReduceSourceCodeAction>().get());
-#endif
+	outs() << "\n============================================================\n";
+	outs() << "VERIFICATION\n";
 	
+	for (const auto& entry : std::filesystem::directory_iterator("temp/"))
+	{
+		if (Validate(userInputError, entry)) break;
+	}
+
+	outs() << "\n============================================================\n";
+
 	return 0;
 }
