227d52f6ea70c9be8454e27957521f4c40eab7d9
leskovde <leskovar.denis@seznam.cz>
2021-04-15 16:54:01 +0200

    Changed the test target to run the test suite

--- autopie/NaiveReduction/test_suite/run_tests.py
+++ autopie/NaiveReduction/test_suite/run_tests.py
@@ -28,7 +28,7 @@ class BaseTest(unittest.TestCase):
         self.inputs_dir = 'tests'
 
         # TODO: Change to the actual location.
-        self.executable_binary = 'build/autopie'
+        self.executable_binary = '../build/bin/NaiveReduction'
 
     def evaluate(self, expected_output, actual_output, command=''):
         """Compares the actual output to the reference output. Prints any differences and fails, if there are any."""
@@ -46,8 +46,8 @@ class BaseTest(unittest.TestCase):
     def runTool(self, filename, expected_out, args):
         """Executes the AutoPIE tool with given arguments. Checks its output and compares it to the reference output."""
 
-        input_path = os.path.join(self.inputs_dir, filename)
-        return_value, actual_output = create_subprocess(self.executable_binary, args + [input_path] + ['--'])
+        #input_path = os.path.join(self.inputs_dir, filename)
+        return_value, actual_output = create_subprocess(self.executable_binary, args + [filename] + ['--'])
         actual_output = actual_output.decode('utf-8')
 
         self.assertEqual(return_value, 0)
@@ -56,10 +56,10 @@ class BaseTest(unittest.TestCase):
 
 def main():
     if not os.path.isdir('tests'):
-        print('ERROR: Invalid directory. Run from the \'tests\' directory.')
+        print('ERROR: Invalid directory. Run from the \'tests_suite\' directory.')
 
-        # if not os.path.isdir('build'):
-            # print('ERROR: The tool has not been built yet. The 'build' directory does not exist.')
+        if not os.path.isdir('../build/bin'):
+            print('ERROR: The tool has not been built yet. The \'build/bin\' directory does not exist.')
 
         sys.exit(1)
     else:
--- autopie/NaiveReduction/test_suite/test_01_example1.py
+++ autopie/NaiveReduction/test_suite/test_01_example1.py
@@ -1,7 +1,7 @@
 import unittest
 from run_tests import BaseTest
 
-TestName = 'example1.c'
+TestName = 'tests/example1.c'
 
 Output = '''\
 '''
@@ -9,7 +9,12 @@ Output = '''\
 
 class Example1(BaseTest):
     def test_execution(self):
-        self.runTool(TestName, Output, [])
+        self.runTool(TestName, Output, ['--loc-file=\"' + TestName + '\"',
+            '--loc-line=4',
+            '--error-message=\"segmentation fault\"',
+            '--ratio=0.5',
+            '--dump-dot=true',
+            '--verbose=true'])
 
 
 if __name__ == '__main__':
