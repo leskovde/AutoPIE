66fdeb2b19e7a6f4e159fb213afebaae09c80309
leskovde <leskovar.denis@seznam.cz>
2021-05-04 23:52:25 +0200

    Changed test extensions

--- autopie/NaiveReduction/test_suite/tests/example9.c
+++ autopie/NaiveReduction/test_suite/tests/example9.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * Unprotected link list references might lead to invalid memory access.
@@ -22,14 +23,14 @@ main()
 	struct Record head;
 	struct Record tail;
 
-	head.str1 = "h_str1";
-	head.str2 = "h_str2";
-	head.str3 = "h.str3";
+	strncpy(head.str1, "h_str1", 20);
+	strncpy(head.str2, "h_str2", 20);
+	strncpy(head.str3, "h.str3", 20);
 	head.link = &tail;
 	
-	tail.str1 = "t_str1";
-	tail.str2 = "t_str2";
-	tail.str3 = "t_str3";
+	strncpy(tail.str1, "t_str1", 20);
+	strncpy(tail.str2, "t_str2", 20);
+	strncpy(tail.str3, "t_str3", 20);
 
 	printf("After head 0: %s\n", head.str1);
 	printf("After head 1: %s\n", head.link->str1);
