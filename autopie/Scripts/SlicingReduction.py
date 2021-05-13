453e028cd5929412388c54979d2fbebeafbefdc2
leskovde <leskovar.denis@seznam.cz>
2021-05-13 15:55:11 +0200

    Added callbacks for all tools and helper scripts

--- autopie/Scripts/SlicingReduction.py
+++ autopie/Scripts/SlicingReduction.py
@@ -5,8 +5,9 @@ import subprocess
 import sys
 import argparse
 from types import SimpleNamespace
+
+import unify
 from slice import run_slicer
-from unify import unify
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--source_file", required=True, type=str, help="The name of the file in which the error occurred.")
@@ -32,6 +33,66 @@ parser.add_argument("--dynamic_slice", type=bool, default=True, help="Runs a pas
 parser.add_argument("--delta", type=bool, default=True, help="Uses the minimizing Delta debugging algorithm before "
                                                              "launching naive reduction. Enabled by default.")
 
+# TODO: Validate that the tools exist.
+delta_path = "../DeltaReduction/build/bin/DeltaReduction"
+naive_path = "../NaiveReduction/build/bin/NaiveReduction"
+var_extractor_path = "../VariableExtractor/build/bin/VariableExtractor"
+slice_extractor_path = "../SliceExtractor/build/bin/SliceExtractor"
+
+unification_output = "unification.txt"
+var_extractor_output = "criteria.txt"
+slice_extractor_output = ["slice.c", "slice.cpp"]
+
+
+def run_unification(slices):
+    if len(slices) < 1:
+        return False
+
+    raw_args = ["--file"]
+
+    for slice in slices:
+        raw_args.append(slice)
+
+    raw_args.append("-o=")
+    raw_args.append(unification_output)
+
+    unification_args = unify.parser.parse_args(raw_args)
+
+    unify.unify(unification_args)
+
+    return True
+
+
+def run_static_slicer(args, var, iteration):
+    output_file = f"static_slice_{iteration}.txt"
+
+    static_slicer_args = SimpleNamespace(
+        file=args.source_file,
+        criterion=var,
+        output=output_file,
+        dynamic_slicer=False
+    )
+
+    run_slicer(static_slicer_args)
+
+    return output_file
+
+
+def run_dynamic_slicer(args, var, iteration):
+    output_file = f"dynamic_slice_{iteration}.txt"
+
+    dynamic_slicer_args = SimpleNamespace(
+        file=args.source_file,
+        criterion=var,
+        output=output_file,
+        arguments=args.arguments,
+        dynamic_slicer=True
+    )
+
+    run_slicer(dynamic_slicer_args)
+
+    return output_file
+
 
 def run_tool(binary_path, arguments):
     print(f"Executing '{binary_path}'...")
@@ -44,57 +105,85 @@ def run_tool(binary_path, arguments):
     return proc.returncode
 
 
-def run_static_slicer(args, var, iteration):
-    static_slicer_args = SimpleNamespace(
-        file=args.source_file,
-        criterion=var,
-        output=f"static_slice_{iteration}.txt",
-        dynamic_slicer=False
-    )
+def run_variable_extraction():
+    var_extractor_args = ["--loc-file=", args.source_file,
+                          "--loc-line=", args.line_number,
+                          "--verbose=", args.verbose,
+                          "--log=", args.log,
+                          "-o=", var_extractor_output
+                          ]
 
-    run_slicer(static_slicer_args)
+    return run_tool(var_extractor_path, var_extractor_args)
 
 
-def run_dynamic_slicer(args, var, iteration):
-    dynamic_slicer_args = SimpleNamespace(
-        file=args.source_file,
-        criterion=var,
-        output=f"dynamic_slice_{iteration}.txt",
-        arguments=args.arguments,
-        dynamic_slicer=True
-    )
+def run_slice_extraction(slice_file):
+    slice_extractor_args = ["--loc-file=", args.source_file,
+                            "--slice-file", slice_file,
+                            "--verbose=", args.verbose,
+                            "--log=", args.log,
+                            "-o=", slice_extractor_output
+                            ]
 
-    run_slicer(dynamic_slicer_args)
+    return run_tool(slice_extractor_path, slice_extractor_args)
 
 
 def run_delta(args):
+    delta_args = ["--loc-file=", args.source_file,
+                  "--loc-line=", args.line_number,
+                  "--error-message=", args.error_message,
+                  "--arguments=", args.arguments,
+                  "--dump-dot=", args.dump_dot,
+                  "--verbose=", args.verbose,
+                  "--log=", args.log
+                  ]
 
-    run_tool()
+    return run_tool(delta_path, delta_args)
+
+
+def run_naive(args):
+    naive_args = ["--loc-file=", args.source_file,
+                  "--loc-line=", args.line_number,
+                  "--error-message=", args.error_message,
+                  "--arguments=", args.arguments,
+                  "--ratio=", args.reduction_ratio,
+                  "--dump-dot=", args.dump_dot,
+                  "--verbose=", args.verbose,
+                  "--log=", args.log  # TODO: Logging twice in a row will remove the previous log. Add a cool-down.
+                  ]
+
+    return run_tool(naive_path, naive_args)
 
 
 def main(args):
     source_code = args.source_file
     adjusted_line = args.line_number
-    variables = []
 
     print(f"Extracting variables...")
+    variables = run_variable_extraction()
 
     if args.static_slice:
         static_slices = []
+        i = 0
+
         for var in variables:
             print(f"Running static slicing with the criterion '{var}'...")
+            static_slices.append(run_static_slicer(args, var, i))
+            i += 1
 
-        unified_slice = None
+        if run_unification(static_slices):
+            print("Extracting source code for the unified slice...")
 
-        print("Extracting source code for the unified slice...")
-
-        source_code = None
-        adjusted_line = None
+            source_code = run_slice_extraction(unification_output)
+            adjusted_line = None
 
     if args.dynamic_slice:
         dynamic_slices = []
+        i = 0
+
         for var in variables:
             print(f"Running dynamic slicing with the criterion '{var}'...")
+            dynamic_slices.append(run_dynamic_slicer(args, var, i))
+            i += 1
 
         unified_slice = None
 
