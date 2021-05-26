dff7604df80996b9cd75753cd1d79bf716c70472
leskovde <leskovde>
2021-05-26 15:47:58 +0200

    Fixed ifs in the script

--- autopie/buildLLVM.sh
+++ autopie/buildLLVM.sh
@@ -31,22 +31,31 @@ esac
 echo "Downloading LLVM 11.0.0. ..."
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/llvm-project-11.0.0.tar.xz
 
+if [ $? -ne 0 ]; then
+        echo "The download failed. Terminating..."
+        cd -
+        exit 1
+fi
+
 temp_dir=$(mktemp -d -t llvm-XXXX)
 echo "Temp dir $temp_dir created."
 
 echo "Extracting the downloaded archive..."
-tar -xvf llvm-project-11.0.0.tar.xz -C $temp_dir
+tar -xf llvm-project-11.0.0.tar.xz -C $temp_dir
+
+echo "Removing the downloaded archive..."
+rm -f llvm-project-11.0.0.tar.xz
 
 echo "Creating the build subdirectory..."
-mkdir $temp_dir/build
+mkdir $temp_dir/llvm-project-11.0.0/build
 
 echo "Switching to the build subdirectory..."
-cd $temp_dir/build
+cd $temp_dir/llvm-project-11.0.0/build
 
 echo "Running CMake..."
 cmake -G Ninja ../llvm -DLLVM_ENABLE_PROJECTS="clang;lldb" -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_USE_LINKER=gold -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_USE_SPLIT_DWARF=ON -DCMAKE_BUILD_TYPE=Release -DLLDB_INCLUDE_TESTS=OFF -DLLDB_ENABLE_PYTHON=1
 
-if [$? -ne 0]; then
+if [ $? -ne 0 ]; then
 	echo "The CMake command failed. Terminating..."
 	cd -
 	exit 1
@@ -55,7 +64,7 @@ fi
 echo "Running ninja build..."
 ninja
 
-if [$? -ne 0]; then
+if [ $? -ne 0 ]; then
         echo "The ninja build failed. Terminating..."
         cd -
         exit 1
@@ -64,7 +73,7 @@ fi
 echo "Running ninja install..."
 ninja install
 
-if [$? -ne 0]; then
+if [ $? -ne 0 ]; then
         echo "The ninja install command failed. Terminating..."
         cd -
         exit 1
